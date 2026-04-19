38271231c603fab131912a5a387148a2eaf14b47
Maestro <otabekavazov2010@gmail.com>
2026-04-19 22:03:14 +0500

    Add files via upload

--- examples/mode.rs
+++ examples/mode.rs
@@ -1,29 +1,75 @@
 use maestro_tgbot::{TelegramBot, bot};
-use serde_json::json;
+use maestro_tgbot::types::*;
+use tokio::time::{sleep, Duration};
 
 #[tokio::main]
 async fn main() {
-    // Bot init
-    let _ = TelegramBot::init("YOUR_BOT_TOKEN");
+    
+    let _ = TelegramBot::init("8520493598:AAFYiGyG_-Lvl7DW5YrTomCKCLEFuZxgsw8");
 
-    // Inline keyboard
-    let keyboard = json!({
-        "inline_keyboard": [
-            [
-                {"text": "✅ ON", "callback_data": "mode_on"},
-                {"text": "❌ OFF", "callback_data": "mode_off"}
-            ]
+    let mut offset = 0;
+    let keyboard = serde_json::json!({
+    "inline_keyboard": [
+        [
+            {"text": "✅ Admin", "url": "t.me/famous_coder"},
+            {"text": "👌 Click", "callback_data": "click"}
         ]
-    }).to_string();
+    ]
+}).to_string();
 
-    // Send message
-    let res = bot!("sendMessage", {
-        "chat_id" => "YOUR_CHAT_ID",
-        "text" => "<b>Select mode:</b>",
-        "parse_mode" => "HTML",
-        "reply_markup" => keyboard
-    });
+    loop {
+        let res = bot!("getUpdates", {
+            "offset" => offset,
+            "timeout" => 10
+        }).unwrap();
 
-    println!("Send result: {:?}", res);
+        let data: Response = serde_json::from_str(&res).unwrap();
+
+        for update in data.result {
+            offset = update.update_id + 1;
+
+            if let Some(message) = update.message {
+                let cid = message.chat.id;
+                let text = message.text.unwrap_or_default();
+                let (first, last) = if let Some(user) = message.from {
+        (
+            user.first_name.unwrap_or_default(),
+            user.last_name.unwrap_or_default(),
+        )
+    } else {
+        ("".to_string(), "".to_string())
+    };
+                let name = format!("{} {}", first, last);
+                if text == "/start" {
+                let _res = bot!("sendMessage", {
+    "chat_id" => cid ,
+    "text" => format!("<b>👋 Hello</b> {}", name),
+    "parse_mode" => "HTML",
+    "reply_markup" => keyboard
+});
+}
+            }
+            if let Some(cb) = update.callback_query {
+    let data = cb.data.unwrap_or_default();
+let cid = cb.message.as_ref().unwrap().chat.id;
+    let mid = cb.message.as_ref().unwrap().message_id;
+    let qid = cb.id;
+    let _ = bot!("answerCallbackQuery", {
+                    "callback_query_id" => qid,
+                    "text" => "OK 👍",
+                    "show_alert"=>true,
+                });
+    if data == "click" {
+    let _ = bot!("editMessageText", {
+            "chat_id" => cid,
+            "message_id" => mid,
+            "text" => "Clicked 👌"
+        });
+    }
+}
+        }
+        
+       sleep(Duration::from_millis(800)).await;
+    }
 
 }
